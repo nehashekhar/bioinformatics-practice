daa2b62e1df502581cd003a72cc55389d0a443c0
nehashekhar <neha.shekhar92@gmail.com>
2016-08-12 19:22:03 -0700

    Add files via upload

---
+++ rosalind/src/main/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelations.java
@@ -0,0 +1,16 @@
+package bioinformatics_practice.rosalind;
+
+/**
+ * This class solves the problem delineated 
+ * here: http://rosalind.info/problems/fib/
+ * @author nehashekhar
+ *
+ */
+public class RabbitsAndRecurrenceRelations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ rosalind/src/test/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelationsTest.java
@@ -0,0 +1,12 @@
+package bioinformatics_practice.rosalind;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RabbitsAndRecurrenceRelationsTest {
+
+	@Test
+	public void fibonacciTest() {
+		Assert.fail("Test to be written");
+	}
+}
