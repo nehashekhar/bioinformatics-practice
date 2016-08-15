23b25eed5a8f5088a3f862e8baa4b7a968a359df
nehashekhar <neha.shekhar92@gmail.com>
2016-08-14 20:15:14 -0700

    Updated RabbitsAndRecurrenceRelations

--- rosalind/src/main/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelations.java
+++ rosalind/src/main/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelations.java
@@ -1,16 +1,64 @@
 package bioinformatics_practice.rosalind;
 
 /**
- * This class solves the problem delineated 
- * here: http://rosalind.info/problems/fib/
+ * This class solves the problem delineated here:
+ * http://rosalind.info/problems/fib/
+ * 
  * @author nehashekhar
  *
  */
 public class RabbitsAndRecurrenceRelations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			int k = defineK();
+			int n = defineN();
+			
+			System.out.println("Fibonacci number: " + fibonacciSequence(k, n));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//This function can be extended to read from another source, e.g. file or user input
+	//Currently, the number is hardcoded
+	public static int defineK() {
+		return 3;
+	}
 
+	//This function can be extended to read from another source, e.g. file or user input
+	//Currently, the number is hardcoded
+	public static int defineN() {
+		return 5;
+	}
+
+	public static int fibonacciSequence(int k, int n) throws Exception {
+		// Calculating the sum of a number requires three numbers:
+		// Fn, Fn-1, Fn-2
+		// Index starts at 0 so that F0=0 and F1=F2=1
+
+		if (n < 0 | n > 40) {
+			throw new Exception("n should be a positive integer less than or equal to 40");
+		}
+
+		if (k < 0 | k > 5) {
+			throw new Exception("k should be a positive integer less than or equal to 5");
+		}
+
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+
+		int Fn = 0;
+
+		for (int i = 0; i <= n; i++) {
+			Fn = fibonacciSequence(k, n - 1) + fibonacciSequence(k, n - 2) * k;
+		}
+
+		return Fn;
 	}
 
 }
--- rosalind/src/test/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelationsTest.java
+++ rosalind/src/test/java/bioinformatics_practice/rosalind/RabbitsAndRecurrenceRelationsTest.java
@@ -6,7 +6,47 @@ import org.testng.annotations.Test;
 public class RabbitsAndRecurrenceRelationsTest {
 
 	@Test
-	public void fibonacciTest() {
-		Assert.fail("Test to be written");
+	public void fibonacciTest() throws Exception {
+		int x = RabbitsAndRecurrenceRelations.fibonacciSequence(3, 5);
+		Assert.assertEquals(x, 19);
 	}
+	
+	@Test
+	public void fibonacciPositiveTest() throws Exception {
+		int x = RabbitsAndRecurrenceRelations.fibonacciSequence(1, 9);
+		Assert.assertEquals(x, 34);
+	}
+	
+	@Test
+	public void fibonacciSeedValueZero() throws Exception {
+		int x = RabbitsAndRecurrenceRelations.fibonacciSequence(1, 0);
+		Assert.assertEquals(x, 0);
+	}
+	
+	@Test
+	public void fibonacciSeedValueOne() throws Exception {
+		int x = RabbitsAndRecurrenceRelations.fibonacciSequence(1, 1);
+		Assert.assertEquals(x, 1);
+	}
+	
+	@Test (expectedExceptions = Exception.class)
+	public void lowNValue() throws Exception {
+		RabbitsAndRecurrenceRelations.fibonacciSequence(1, -1);
+	}
+	
+	@Test (expectedExceptions = Exception.class)
+	public void highNValue() throws Exception {
+		RabbitsAndRecurrenceRelations.fibonacciSequence(1, 41);
+	}
+	
+	@Test (expectedExceptions = Exception.class)
+	public void lowKValue() throws Exception {
+		RabbitsAndRecurrenceRelations.fibonacciSequence(-1, 1);
+	}
+	
+	@Test (expectedExceptions = Exception.class)
+	public void highKValue() throws Exception {
+		RabbitsAndRecurrenceRelations.fibonacciSequence(6, 1);
+	}
+
 }
