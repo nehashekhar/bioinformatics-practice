f9ba19139922f5b9d35d467b7a2333a622bbe2e9
nehashekhar <neha.shekhar92@gmail.com>
2016-08-14 22:43:21 -0700

    Added more methods; solution is not yet yielding correct result

--- rosalind/src/main/java/bioinformatics_practice/rosalind/WrightFisherModel.java
+++ rosalind/src/main/java/bioinformatics_practice/rosalind/WrightFisherModel.java
@@ -2,15 +2,74 @@ package bioinformatics_practice.rosalind;
 
 public class WrightFisherModel {
 
-	
-	static int factorial(int N) throws Exception{
+	public static void main(String[] args) {
+		try {
+			int k = 1;
+			int N = 4;
+			int m = 6;
+			int g = 2;
+			double p = 1 - calculateProbability(N, m);
+			double y = atLeastKCopiesOverGenerations(k, N, p, g);
+			System.out.println("The probability of finding at least " + k + " copy(ies)" +
+			" of this gene in a population of " + N + " individuals over " 
+		+ g + " generations is with an initial probability of " + p + " is: " + y);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static double atLeastKCopiesOverGenerations(int k, int N, double p, int g) throws Exception {
+		double x = 0;
+		for (int i = k; i <= (2*N); i++) {
+			x = x + WFMoverGenerations(i, N, p, g);
+		}
+		return x;
+	}
+
+	/*
+	 * This function calculates the WFM probability over g generations
+	 */
+	static double WFMoverGenerations(int k, int N, double p, int g) throws Exception {
+		double probabilityOverGenerations = p;
+
+		for (int i = 0; i <= g; i++) {
+			probabilityOverGenerations = WrightFisherModel(k, N, probabilityOverGenerations);
+		}
+
+		return probabilityOverGenerations;
+	}
+
+	/*
+	 * This function returns the probability of finding k copies of an allele N
+	 * - number of diploid individuals in population p - frequency of one allele
+	 * in previous generation q - frequency of other allele in previous
+	 * generation k - expected copies of an allele found in a generation
+	 */
+	static double WrightFisherModel(int k, int N, double p) throws Exception {
+		double q = 1 - p;
+
+		double numerator = factorial(2 * N) * Math.pow(p, k) * Math.pow(q, (2 * N - k));
+		double denominator = factorial(k) * factorial(2 * N - k);
+
+		return numerator / denominator;
+	}
+
+	/*
+	 * This function calculates the factorial of a number
+	 */
+	static int factorial(int N) throws Exception {
 		if (N > 1) {
-			N = N * factorial(N-1);
+			N = N * factorial(N - 1);
 			return N;
-		} else if (N==0 | N==1) {
+		} else if (N == 0 | N == 1) {
 			return 1;
 		} else {
 			throw new Exception("Factorial for " + N + " is undefined.");
 		}
+	}
+
+	static double calculateProbability(int N, int m) {
+		double p = ((double) m) / ((double) (N * 2));
+		return p;
 	}
 }
--- rosalind/src/test/java/bioinformatics_practice/rosalind/WrightFisherModelTest.java
+++ rosalind/src/test/java/bioinformatics_practice/rosalind/WrightFisherModelTest.java
@@ -7,6 +7,13 @@ import junit.framework.Assert;
 public class WrightFisherModelTest {
 
 	@Test
+	public void WFMoverGTest() throws Exception {
+//		double x = WrightFisherModel.WFMoverGenerations(1, 4, 1 - WrightFisherModel.calculateProbability(4, 6), 2);
+//		Assert.assertEquals(0.772, x);
+		Assert.fail("TBD");
+	}
+	
+	@Test
 	public void factorialTest() throws Exception {
 		int x = WrightFisherModel.factorial(5);
 		Assert.assertEquals(120, x);
@@ -27,5 +34,11 @@ public class WrightFisherModelTest {
 	@Test (expectedExceptions = Exception.class)
 	public void factorialNegativeNumber() throws Exception {
 		WrightFisherModel.factorial(-1);
+	}
+	
+	@Test
+	public void calculateProbabilityTest() {
+		double p = WrightFisherModel.calculateProbability(2, 1);
+		Assert.assertEquals(0.25, p);
 	}
 }
