4a6b6a7f757164caf97ad62e1f3cab5325ea0cfe
nehashekhar <neha.shekhar92@gmail.com>
2016-08-15 09:37:32 -0700

    Made changes to second method of calculating probability

--- rosalind/src/main/java/bioinformatics_practice/rosalind/WrightFisherModel.java
+++ rosalind/src/main/java/bioinformatics_practice/rosalind/WrightFisherModel.java
@@ -101,57 +101,72 @@ public class WrightFisherModel {
 
 	private static double probabilityOverGenerationsAndCopyNumbers(int k, int N, double p, int g) throws Exception {
 		System.out.println("------");
-		//For each generation, we have allelelic frequency given alleleic frequency of previous generation
-		//Loop over alleleic frequencies by generation
-		//E.g. g0=p0, g1=p|p0, p2|p0, p3|p0, etc. until pN
-		//g2=p|p0 from g1, p|p1 from g1, etc.
-		//g3=p|p0 from g2
+		// For each generation, we have allelelic frequency given alleleic
+		// frequency of previous generation
+		// Loop over alleleic frequencies by generation
+		// E.g. g0=p0, g1=p|p0, p2|p0, p3|p0, etc. until pN
+		// g2=p|p0 from g1, p|p1 from g1, etc.
+		// g3=p|p0 from g2
 
-		//HashMap where key=number of alleles in a generation, p=probability of getting k in a generation (changes generationally)
+		// HashMap where key=number of alleles in a generation, p=probability of
+		// getting k in a generation (changes generationally)
 		HashMap<Integer, Double> allelicFreqVsProbability = new HashMap<Integer, Double>();
-		
-		//First generation
+
+		// First generation
 		double g1sum = 0;
-		for (int allelicFreq = 0; allelicFreq<=2*N; allelicFreq++) {
+		for (int allelicFreq = 0; allelicFreq <= 2 * N; allelicFreq++) {
 			double pN = wrightFisherModel(allelicFreq, N, p);
-			System.out.println("G1 probability of getting " + allelicFreq + " copies given p(previous gen) = " + p + "| " + pN );
+			System.out.println(
+					"G1 probability of getting " + allelicFreq + " copies given p(previous gen) = " + p + "| " + pN);
 			allelicFreqVsProbability.put(allelicFreq, pN);
 			g1sum = g1sum + pN;
 		}
 		System.out.println("G1SUM: " + g1sum);
-		
-		//Second generation
-		double g2sum = 0;
-		for (int allelicFreq = 0; allelicFreq<=2*N; allelicFreq++) {
-			double pPreviousGen = allelicFreqVsProbability.get(allelicFreq);
-			double pN = wrightFisherModel(k, N, pPreviousGen);				
-			System.out.println("G2 probability of getting " + k +  " copies given previous gen had " + allelicFreq + " copies | " + pN );
-			allelicFreqVsProbability.put(allelicFreq, pN);
-			g2sum = g2sum + pN;
+
+		// Second generation
+		for (int k2 = 1; k2 <= 2 * N; k2++) {
+			double pN2 =0;
+			for (int allelicFreq = 1; allelicFreq <= 2 * N; allelicFreq++) {
+				double pPreviousGen = allelicFreqVsProbability.get(allelicFreq);
+				double pN = wrightFisherModel(k2, N, calculateProbability(N, allelicFreq));
+				System.out.println("G2 probability of getting " + k2 + " copies given previous gen had " + allelicFreq
+						+ " copies and p " + pPreviousGen + " | " + pN);
+				//TODO: check this addition
+				//Probability of getting X copies (variable) given Y in previous gen (fixed) sums to 1
+				//NOT probability of getting X copies (fixed) given Y in previous gen (variable)
+				//Weighted sum?
+				pN2 = pN2 + pN;
+			}
+			allelicFreqVsProbability.put(k2, pN2);
+			System.out.println("Cumulative probability of getting " + k2 + " copies: " + pN2);
 		}
 		
-		System.out.println("G2SUM: " + g2sum);
+
+		System.out.println("allelicFreqVsProbability.get(k): " + allelicFreqVsProbability.get(k));
+		return allelicFreqVsProbability.get(k);
 
 		// Now iterate over all the possibly allelic frequencies in subsequent
 		// generations
-		double cumulativeProbability = 0;
-//		for (int copyNumber = 0; copyNumber <= 2 * N; copyNumber++) {
-//			System.out.println(copyNumber);
-//			for (int generation = 1; generation <= g; generation++) {
-//				double[] probabilitiesOverOneValueOfM = new double[2*N];
-//				probabilitiesOver2N[copyNumber] = wrightFisherModel(copyNumber, N, probabilitiesOver2N[copyNumber]);
-//				System.out.println(probabilitiesOver2N[copyNumber]);
-//			}
-//			if (copyNumber>=k) {
-//				cumulativeProbability = cumulativeProbability + probabilitiesOver2N[copyNumber];
-//			}
-//
-//			System.out.println("------");
-//		}
+		// double cumulativeProbability = 0;
+		// for (int copyNumber = 0; copyNumber <= 2 * N; copyNumber++) {
+		// System.out.println(copyNumber);
+		// for (int generation = 1; generation <= g; generation++) {
+		// double[] probabilitiesOverOneValueOfM = new double[2*N];
+		// probabilitiesOver2N[copyNumber] = wrightFisherModel(copyNumber, N,
+		// probabilitiesOver2N[copyNumber]);
+		// System.out.println(probabilitiesOver2N[copyNumber]);
+		// }
+		// if (copyNumber>=k) {
+		// cumulativeProbability = cumulativeProbability +
+		// probabilitiesOver2N[copyNumber];
+		// }
+		//
+		// System.out.println("------");
+		// }
 
 		// After iteration, sum the values of the desired allelic frequency and
 		// above
-		return cumulativeProbability;
+		// return cumulativeProbability;
 	}
 
 }
