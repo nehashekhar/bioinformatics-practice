cc5ee42c7dd0e8198f513cfe9fdbfaee80569923
Neha Shekhar <neha@madhu-shekhar.com>
2016-12-20 23:43:21 -0800

    Moved method to utility class

--- rosalind/src/main/java/bioinformatics_practice/rosalind/ComplementingStrandOfDna.java
+++ rosalind/src/main/java/bioinformatics_practice/rosalind/ComplementingStrandOfDna.java
@@ -1,5 +1,7 @@
 package bioinformatics_practice.rosalind;
 
+import bioinformatics_practice.utility.NucleotideUtil;
+
 /**
  * Reversing and finding the complement of a nucleotide sequence
  * http://rosalind.info/problems/revc/
@@ -9,42 +11,13 @@ package bioinformatics_practice.rosalind;
  */
 public class ComplementingStrandOfDna {
 
-	//TODO: Tests for these methods
 	public static void main(String[] args) {
 		String sequence = "AAAACCCGGT";
 		String sC = "";
 
-		for (int i = sequence.length() - 1; i >= 0; i--) {
-			char n = sequence.charAt(i);
-			char nC = returnComplement(n);
-			sC = sC + nC;
-		}
+		sC = NucleotideUtil.returnDnaComplement(sequence);
 
 		System.out.println("The reverse complement of " + sequence + " is " + sC);
 	}
 
-	public static char returnComplement(char n) {
-		char complement = 'N';
-
-		switch (n) {
-		case 'A': {
-			complement = 'T';
-			break;
-		}
-		case 'C': {
-			complement = 'G';
-			break;
-		}
-		case 'G': {
-			complement = 'C';
-			break;
-		}
-		case 'T': {
-			complement = 'A';
-			break;
-		}
-		}
-
-		return complement;
-	}
 }
